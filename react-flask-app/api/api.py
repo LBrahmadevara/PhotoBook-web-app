854edda2bb6c55b7926d8f39b35f8a7b9844d0b0
Likhitha Brahmadevara <likhithagupthalucky@gmail.com>
2020-10-04 23:05:28 -0700

    Calling Vision API using cloud storage images

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -2,6 +2,7 @@ import time
 from flask import Flask, request
 import os
 from google.cloud import storage
+from google.cloud import vision
 
 app = Flask(__name__)
 
@@ -9,8 +10,11 @@ CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
 Service_key = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
 
 @app.route('/upload', methods=['POST'])
-def demo():
+def upload():
     uploaded_file=request.files.get('file')
+
+    # Instantiates a client
+    vision_client = vision.ImageAnnotatorClient()
 
     # Create a Cloud Storage client.
     gcs = storage.Client()
@@ -25,6 +29,23 @@ def demo():
         uploaded_file.read(),
         content_type=uploaded_file.content_type
     )
+
+    # Make the blob publicly viewable.
+    blob.make_public()
+    # print(blob.name)
+
+    # Use the Cloud Vision client to detect a face for our image.
+    source_uri = "gs://{}/{}".format(CLOUD_STORAGE_BUCKET, blob.name)
+    print(source_uri)
+    image = vision.Image(source=vision.ImageSource(gcs_image_uri=source_uri))
+
+    # Performs label detection on the image file
+    labels = vision_client.label_detection(image=image).label_annotations
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+    # print(len(labels))
 
     return blob.public_url
 
